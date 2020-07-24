edb701f42eea0777c77467b91d6a640630041681
mayuresh deolekar <mayureshdeolekar.md@gmail.com>
2020-07-24 23:58:55 +0530

    main.js

--- Vue Instance/main.js
+++ Vue Instance/main.js
@@ -3,8 +3,7 @@ var app = new Vue({
     data: {
         greetings: "Hello World!",
         description: "Hey, My name is Mayuresh",
-    }
-
+    },
     methods: {
         job: function() {
             return "I'm a Software Developer";
