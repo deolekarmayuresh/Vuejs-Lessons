4c03e0f48f34392b24d9a1b62eee0f4b21647866
mayuresh deolekar <mayureshdeolekar.md@gmail.com>
2020-07-24 23:18:56 +0530

    vue instance

--- Vue Instance/main.js
+++ Vue Instance/main.js
@@ -1,6 +1,7 @@
 var app = new Vue({
     el: '#app',
     data: {
-        greetings: "Hello World"
+        greetings: "Hello World",
+        description: "Hey, My name is Mayuresh. I'm a software Developer."
     }
 })
