eede1a4d15eda858c73b34c1780541aac4fb57b9
mayuresh deolekar <mayureshdeolekar.md@gmail.com>
2020-07-25 16:14:03 +0530

    list rendering

---
+++ 4. List Rendering/main.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+        inStock: true,
+        onSale: true,
+        link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
+        details: ["70% cotton", "30% polyster"]
+    }
+})
