8b67e627d26eeccca7b80dd8d9fc102ac912a918
mayuresh deolekar <mayureshdeolekar.md@gmail.com>
2020-07-31 12:59:26 +0530

    app.js

--- 6.  Vue Components Basics/app.js
+++ 6.  Vue Components Basics/app.js
@@ -1,4 +1,12 @@
+Vue.component('my-comp', {
+    data: function() {
+        return {
+            status: 'Error 404'
+        }
+    },
 
+    template: '<p> Server Status: {{ ststus }} </p>'
+});
 
 new Vue({
     el: '#app'
