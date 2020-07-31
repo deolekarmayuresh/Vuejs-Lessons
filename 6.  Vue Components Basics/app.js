7bd1d370a3596aba39719226b81f87f3de77f198
mayuresh deolekar <mayureshdeolekar.md@gmail.com>
2020-07-31 13:13:00 +0530

    app.js

--- 6.  Vue Components Basics/app.js
+++ 6.  Vue Components Basics/app.js
@@ -1,11 +1,19 @@
+let data = { status: 'Error 404'};
+
 Vue.component('my-comp', {
     data: function() {
         return {
             status: 'Error 404'
         }
+        //return data;
     },
 
-    template: '<p> Server Status: {{ status }} </p>'
+    template: '<p> Server Status: {{ status }} <button @click="changeStatus">Change</button> </p>',
+    methods: {
+        changeStatus: function() {
+            this.status = 'OK 200';
+        }
+    },
 });
 
 new Vue({
