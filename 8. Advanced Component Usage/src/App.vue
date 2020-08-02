2b1921ff0c0035f2be4a85c6bca75580b32e4860
mayuresh deolekar <mayureshdeolekar.md@gmail.com>
2020-08-02 13:34:49 +0530

    advance-component-usage

---
+++ 8. Advanced Component Usage/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+              <button @click="selectedComponent = 'appQuote'">Quote</button>
+              <button @click="selectedComponent = 'appAuthor'">Author</button>
+              <button @click="selectedComponent = 'appNew'">New</button>
+              <hr>
+              <keep-alive>
+                <component :is="selectedComponent">
+                  <p>Default Content</p>
+                </component>
+              </keep-alive>
+              
+               <!-- <app-quote>
+                 <h2 slot="title"> {{ quoteTitle }} </h2>
+                 <p slot="content">A Wonderful Quote!</p>
+               </app-quote> -->
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import  Quote  from "./components/Quote";
+    import  Author  from "./components/Author";
+    import  New  from "./components/New";
+
+    export default {
+        data() {
+          return {
+            quoteTitle: 'The Quote',
+            selectedComponent: 'appQuote'
+          }
+        },
+        components: {
+          appQuote: Quote,
+          appAuthor: Author,
+          appNew: New
+        }
+    }
+</script>
+
+<style>
+</style>
